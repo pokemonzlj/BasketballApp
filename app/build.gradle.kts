1b8f39ee60d59d6e1fc87c839750c45f6854cb15
叫我_叼哥 <35096840+pokemonzlj@users.noreply.github.com>
2026-07-03 10:19:53 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,4 +50,5 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("mysql:mysql-connector-java:8.0.33")
 }
